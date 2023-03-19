dda74cd846ea888d2f768247070fa620a35948a8
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-19 23:45:19 +0100

    Remove duplicate lines [build]

--- src/main/java/com/mrbysco/densetrees/data/data/DenseItemTagProvider.java
+++ src/main/java/com/mrbysco/densetrees/data/data/DenseItemTagProvider.java
@@ -18,8 +18,5 @@ public class DenseItemTagProvider extends ItemTagsProvider {
 		this.tag(ItemTags.LOGS).add(DenseRegistry.DENSE_OAK_LOG_ITEM.get(), DenseRegistry.DENSE_SPRUCE_LOG_ITEM.get(),
 				DenseRegistry.DENSE_BIRCH_LOG_ITEM.get(), DenseRegistry.DENSE_JUNGLE_LOG_ITEM.get(),
 				DenseRegistry.DENSE_ACACIA_LOG_ITEM.get(), DenseRegistry.DENSE_DARK_OAK_LOG_ITEM.get());
-		this.tag(ItemTags.LOGS).add(DenseRegistry.DENSE_OAK_LOG_ITEM.get(), DenseRegistry.DENSE_SPRUCE_LOG_ITEM.get(),
-				DenseRegistry.DENSE_BIRCH_LOG_ITEM.get(), DenseRegistry.DENSE_JUNGLE_LOG_ITEM.get(),
-				DenseRegistry.DENSE_ACACIA_LOG_ITEM.get(), DenseRegistry.DENSE_DARK_OAK_LOG_ITEM.get());
 	}
 }
