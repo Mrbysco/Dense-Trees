ff2a68bb6244efad61b664e5eb792e44b0b95434
Mrbysco <Mrbysco@users.noreply.github.com>
2023-06-05 16:23:44 +0200

    Add the dense crimson/warped stems to the appropriate tags (Fixes #2)

--- src/main/java/com/mrbysco/densetrees/data/data/DenseBlockTagProvider.java
+++ src/main/java/com/mrbysco/densetrees/data/data/DenseBlockTagProvider.java
@@ -22,6 +22,9 @@ public class DenseBlockTagProvider extends BlockTagsProvider {
 		this.tag(BlockTags.LOGS_THAT_BURN).add(DenseRegistry.DENSE_OAK_LOG.get(), DenseRegistry.DENSE_SPRUCE_LOG.get(),
 				DenseRegistry.DENSE_BIRCH_LOG.get(), DenseRegistry.DENSE_JUNGLE_LOG.get(),
 				DenseRegistry.DENSE_ACACIA_LOG.get(), DenseRegistry.DENSE_DARK_OAK_LOG.get(), DenseRegistry.DENSE_MANGROVE_LOG.get());
+		this.tag(BlockTags.CRIMSON_STEMS).add(DenseRegistry.DENSE_CRIMSON_STEM.get());
+		this.tag(BlockTags.WARPED_STEMS).add(DenseRegistry.DENSE_WARPED_STEM.get());
+		this.tag(BlockTags.NON_FLAMMABLE_WOOD).add(DenseRegistry.DENSE_CRIMSON_STEM.get(), DenseRegistry.DENSE_WARPED_STEM.get()); //TODO: Remove in 1.19.3 as the tag is removed there
 		this.tag(BlockTags.MINEABLE_WITH_AXE).add(DenseRegistry.DENSE_OAK_LOG.get(), DenseRegistry.DENSE_SPRUCE_LOG.get(),
 				DenseRegistry.DENSE_BIRCH_LOG.get(), DenseRegistry.DENSE_JUNGLE_LOG.get(),
 				DenseRegistry.DENSE_ACACIA_LOG.get(), DenseRegistry.DENSE_DARK_OAK_LOG.get(), DenseRegistry.DENSE_MANGROVE_LOG.get());
--- src/main/java/com/mrbysco/densetrees/data/data/DenseItemTagProvider.java
+++ src/main/java/com/mrbysco/densetrees/data/data/DenseItemTagProvider.java
@@ -5,6 +5,7 @@ import com.mrbysco.densetrees.registry.DenseRegistry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -19,5 +20,8 @@ public class DenseItemTagProvider extends ItemTagsProvider {
 				DenseRegistry.DENSE_BIRCH_LOG_ITEM.get(), DenseRegistry.DENSE_JUNGLE_LOG_ITEM.get(),
 				DenseRegistry.DENSE_ACACIA_LOG_ITEM.get(), DenseRegistry.DENSE_DARK_OAK_LOG_ITEM.get(),
 				DenseRegistry.DENSE_MANGROVE_LOG_ITEM.get());
+		this.tag(ItemTags.CRIMSON_STEMS).add(DenseRegistry.DENSE_CRIMSON_STEM_ITEM.get());
+		this.tag(ItemTags.WARPED_STEMS).add(DenseRegistry.DENSE_WARPED_STEM_ITEM.get());
+		this.tag(ItemTags.NON_FLAMMABLE_WOOD).add(DenseRegistry.DENSE_CRIMSON_STEM_ITEM.get(), DenseRegistry.DENSE_WARPED_STEM_ITEM.get()); //TODO: Remove in 1.19.3 as the tag is removed there
 	}
 }
